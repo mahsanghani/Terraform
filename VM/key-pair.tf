32ccc2147152b1b680d715054625cfdfd704cf5b
mahsanghani <ghaniahsan@outlook.com>
2022-08-04 13:47:41 -0700

    key-pair

---
+++ VM/key-pair.tf
@@ -0,0 +1,14 @@
+resource "tls_private_key" "key_pair" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "key_pair" {
+  key_name   = "${lower(var.app_name)}-${lower(var.app_environment)}-linux-${lower(var.aws_region)}"
+  public_key = tls_private_key.key_pair.public_key_openssh
+}
+
+resource "local_file" "ssh_key" {
+  filename = "${aws_key_pair.key_pair.key_name}.pem"
+  content  = tls_private_key.key_pair.private_key_pem
+}
